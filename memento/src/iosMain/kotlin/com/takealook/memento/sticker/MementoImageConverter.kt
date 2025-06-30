cde2f8350fed56ecbcd8fee5b18ff2b2ba10cab6
easternkite <easternkite7@gmail.com>
2025-06-30 21:06:36 +0900

    [takea-look#12] remove useless imports

--- memento/src/iosMain/kotlin/com/takealook/memento/sticker/MementoImageConverter.kt
+++ memento/src/iosMain/kotlin/com/takealook/memento/sticker/MementoImageConverter.kt
@@ -1,11 +1,8 @@
 package com.takealook.memento.sticker
 
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
-import org.jetbrains.skia.Image
 import platform.UIKit.UIImage
 import platform.UIKit.UIImagePNGRepresentation
 import platform.posix.memcpy
