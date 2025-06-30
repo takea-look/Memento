6f4847b49fa81cb181b01deee4317e3a1c1ff05d
easternkite <easternkite7@gmail.com>
2025-07-01 00:20:01 +0900

    [takea-look#13] remove useless import

--- memento/src/commonMain/kotlin/com/takealook/memento/MementoEditor.kt
+++ memento/src/commonMain/kotlin/com/takealook/memento/MementoEditor.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
